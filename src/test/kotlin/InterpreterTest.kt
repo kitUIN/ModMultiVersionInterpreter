93011e8612bb2507edb0d33cd28c8474c6441a52
kituin <kit_uin@outlook.com>
2025-02-16 15:11:09 +0800

    :sparkles: Support for wildcards * for loaders

--- src/test/kotlin/InterpreterTest.kt
+++ src/test/kotlin/InterpreterTest.kt
@@ -20,9 +20,37 @@ class InterpreterTest {
         assertTrue(interpreter.interpret())
     }
     @Test
+    fun run25() {
+        val interpreter = Interpreter(">=fabric-1.20", mutableMapOf(
+            "$$" to "fabric-1.20.3"
+        ))
+        assertTrue(interpreter.interpret())
+    }
+    @Test
     fun run3() {
         val interpreter = Interpreter("$$ == fabric-1.16.5", mutableMapOf(
             "$$" to "fabric-1.16.5"
+        ))
+        assertTrue(interpreter.interpret())
+    }
+    @Test
+    fun run4() {
+        val interpreter = Interpreter("fabric-1.20", mutableMapOf(
+            "$$" to "fabric-1.20.3"
+        ))
+        assertFalse(interpreter.interpret())
+    }
+    @Test
+    fun run5() {
+        val interpreter = Interpreter("*-1.20", mutableMapOf(
+            "$$" to "fabric-1.20"
+        ))
+        assertTrue(interpreter.interpret())
+    }
+    @Test
+    fun run6() {
+        val interpreter = Interpreter(">=*-1.20", mutableMapOf(
+            "$$" to "forge-1.20.3"
         ))
         assertTrue(interpreter.interpret())
     }
